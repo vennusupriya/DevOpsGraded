5714844d3768fcd2c3744ee2a6efa637f51ba358
vennu susmitha <vennu.supriya@hcl.com>
2021-11-23 02:56:00 -0500

    solution

---
+++ DevOps/src/main/java/com/hcl/devopsgraded/DevOpsApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.devopsgraded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
